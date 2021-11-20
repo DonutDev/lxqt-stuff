02f26c117e7f9740fac3653a352ae82652aa9a9a
DonutDev <58142952+DonutDev@users.noreply.github.com>
2021-11-20 19:54:07 +0200

    added element and ffmpeg

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 sudo dnf upgrade
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install *compiz* ccsm vim krita git discord steam neofetch htop kvantum mpv compton --allowerasing
+sudo dnf install *compiz* ccsm vim krita git discord steam neofetch htop kvantum mpv compton ffmpeg --allowerasing
+sudo dnf remove openbox
+
+sudo dnf install -y dnf-plugins-core distribution-gpg-keys
+‍
+sudo dnf copr enable taw/element
+‍
+sudo dnf install -y element --refresh
 
 echo "Run this script by doing . ./lx.sh DON'T run it as ./lx.sh as it won't run"
 echo "Stay close to the computer and don't do anything until the script is finished"
