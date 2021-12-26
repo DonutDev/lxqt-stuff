da58a025b66580bc25edf7767b25e40517c1a85e
DonutDev <58142952+DonutDev@users.noreply.github.com>
2021-12-26 11:52:55 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 sudo dnf upgrade
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install *compiz* ccsm vim krita git discord steam neofetch htop kvantum mpv compton ffmpeg python3-pip qt5-designer alacritty flameshot firefox --allowerasing 
-sudo dnf remove pavucontrol falkon
+sudo dnf remove pavucontrol falkon qterminal
 
 echo "Run this script by doing . ./lx.sh DON'T run it as ./lx.sh as it won't run"
 echo "Stay close to the computer and don't do anything until the script is finished"
